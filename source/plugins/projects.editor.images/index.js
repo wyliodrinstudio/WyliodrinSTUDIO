770fc20e27f434c2328384cf85e63a519c468294
Diana Ghindaoanu <diana_ghindaoanu@yahoo.com>
2019-09-18 12:32:41 +0300

    image editor

--- source/plugins/projects.editor.images/index.js
+++ source/plugins/projects.editor.images/index.js
@@ -3,7 +3,7 @@ import ImageEditor from './views/ImageEditor.vue';
 export default function setup (options, imports, register)
 {
 	const studio = imports;
-	studio.projects.registerEditor('EDITOR_IMAGE',['png', 'jpg', 'jpeg', 'gif', 'svg'], ImageEditor);
+	studio.projects.registerEditor('EDITOR_IMAGE',['png', 'jpg', 'jpeg', 'gif', 'svg', 'tiff', 'bmp', 'ppm', 'pgm', 'pbm', 'pnm', 'bat', 'bpg'], ImageEditor);
 	
 	register (null, {});
 }
