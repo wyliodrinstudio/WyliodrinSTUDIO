68ef4971dcf6f4a80df543539babb0afcaa5236c
cosmindanielradu19 <cosmindanielradu19@gmail.com>
2020-01-06 17:51:12 +0200

    fixed cors

--- source/plugins/ctf.admin/routes/front.js
+++ source/plugins/ctf.admin/routes/front.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 import db from '../functions/database';
+import qs from '../views/QuestionsFront.vue';
+import { render } from 'katex';
 
 const router = express.Router();
 
@@ -8,5 +10,16 @@ router.get('/', (req, res) => {
 	res.send('Testare ruta front').sendStatus(200);
 });
 
+router.get('/questions', (req, res) => {
+	try {
+		console.log(qs);
+		//res.send(qs).sendStatus(200);
+		res.render(qs);
+	} catch (err) {
+		res.send({
+			err: "There was a problem fetching the questions from database"
+		})
+	}
+}); 
 
 export default router;
