9b9aa819c68a3330345107b5870ffa9597ae2d2c
skyplane23 <moraruliviunicolae1@gmail.com>
2020-01-08 19:16:32 +0200

    string & js options added

--- source/plugins/ctf.admin/views/Questions.vue
+++ source/plugins/ctf.admin/views/Questions.vue
@@ -11,35 +11,59 @@
 		<v-card-text>
 			<v-list>
 				<v-list-group
-				v-for="item in questions"
-				:key="item.ID"
-				v-model="item.active"
-				no-action
+					v-for="item in questions"
+					:key="item.ID"
+					v-model="item.active"
+					no-action
 				>
 				<template v-slot:activator>
 					<v-list-item-content>
-					<v-list-item-title v-text="item.Question"></v-list-item-title>
+						<v-list-item-title v-text="item.Question"></v-list-item-title>
 					</v-list-item-content>
 				</template>
 		
 				<v-list-item>
 					<v-list-item-content>
+						<span class="subtitle-2 mb-n4">-> Question</span>
 						<v-text-field
 							v-model="item.Question"
-							label="Question"
 						></v-text-field>
-						<v-text-field
-							v-model="item.Answer"
-							label="Answer"
-						></v-text-field>
+						<span class="subtitle-2 mb-1">-> Answer</span>
+						<v-tabs 
+							v-model="item.answerTab"
+							background-color="transparent"
+							height=20
+							grow
+						>
+							<v-tab v-for="tab in item.answerTabOptions" :key="tab.key"> {{tab.text}} </v-tab>
+						</v-tabs>
+						<v-tabs-items
+							v-model="item.answerTab"
+							grow
+							class="mt-1"
+						>
+							<v-tab-item v-for="tab in item.answerTabOptions" :key="tab.key" @click="infoLog">
+								<v-text-field
+									v-model="tab.content"
+									v-if="tab.key === 'string'"
+								></v-text-field>
+								<MonacoEditor 
+									style="min-height: 100px" 
+									v-model="tab.content" 
+									:options="editorOptions" 
+									language="javascript"
+									v-else
+								></MonacoEditor>
+							</v-tab-item>
+						</v-tabs-items>
+						<span class="subtitle-2 mb-n4">-> Score</span>
 						<v-text-field
 							v-model="item.Score"
-							label="Score"
 							type="number"
 						></v-text-field>
+						<span class="subtitle-2 mb-n4">-> Parent</span>
 						<v-text-field
-							v-model="item.LockedBy"
-							label="Locked By"
+							v-model="item.Parent"
 							type="number"
 						></v-text-field>
 						<v-spacer></v-spacer>
@@ -62,47 +86,116 @@
 	</v-card>
 </template>
 
-<script>
-  export default {
-	props: ['width', 'activeDb'],
-	data () {
-		return {
-			questions: [],
-			deletedQuestions: []
-		}
-	},
-	async created() {
-		this.questions = this.questions.concat(await this.studio.ctf_admin.getAllQuestions(this.activeDb + '.sqlite'));
-	},
-	methods: {
-		closeDialog ()
-		{
-			this.studio.ctf_admin.updateDbQuestions(this.questions, this.deletedQuestions, this.activeDb + '.sqlite');
-			this.$root.$emit('submit');
-		},
-		addQuestion () {
-			this.questions.push({
-				ID: this.questions.length + 1,
-				Question: 'Question ' + (this.questions.length + 1),
-				Answer: 'Answer ' + (this.questions.length + 1),
-				Score: 0,
-				LockedBy: -1,
-				newQuestion: true
-			})
-		},
-		deleteQuestion () {
-			this.questions.forEach((item, idx) => {
-				if (item.active) {
-					this.questions.splice(idx, 1);
+<style>
+	.v-window__container  {
+		background: transparent !important;
+		background-color: transparent !important;
+	}
+	.theme--light.v-tabs-items {
+		background: transparent !important;
+		background-color: transparent !important;
+		width: 100% !important;
+	}
+</style>
 
-					if (!item.newQuestion) {
-						this.deletedQuestions.push(item.ID);
-					}
+<script>
+	import MonacoEditor from 'vue-monaco';
+	import * as monaco from 'monaco-editor';
+
+	export default {
+		props: ['width', 'activeDb'],
+		data () {
+			return {
+				questions: [],
+				deletedQuestions: [],
+				editorOptions: {
+					fontSize: 13,
+					automaticLayout: true
 				}
+			}
+		},
+		components: {
+			MonacoEditor
+		},
+		async created() {
+			(await this.studio.ctf_admin.getAllQuestions(this.activeDb + '.sqlite')).forEach((item) => {
+				item.answerTabOptions = [
+					{
+						text: 'String',
+						key: 'string',
+						content: ''
+					},
+					{
+						text: 'JS Function',
+						key: 'jsFunction',
+						content: ''
+					}
+				];
+				item.answerTabOptions[item.AnswerType].content = item.Answer;
+				item.answerTab = item.AnswerType;
+
+				this.questions.push(item);
 			})
-			console.log(this.questions);
-			console.log(this.deletedQuestions);
+
+
+			console.log(this.questions)
+			this.$forceUpdate ();
+		},
+		methods: {
+			closeDialog ()
+			{
+				this.questions.map((item) => {
+					item.AnswerType = item.answerTab;
+					item.Answer = item.answerTabOptions[item.answerTab].content;
+
+					return item;
+				})
+
+				console.log(this.deletedQuestions);
+
+				this.studio.ctf_admin.updateDbQuestions(this.questions, this.deletedQuestions, this.activeDb + '.sqlite');
+				this.$root.$emit('submit');
+			},
+			addQuestion () {
+				let question = {
+					ID: this.questions.length + 1,
+					Question: 'Question ' + (this.questions.length + 1),
+					answerTab: 0,
+					answerTabOptions: [
+						{
+							text: 'String',
+							key: 'string',
+							content: ''
+						},
+						{
+							text: 'JS Function',
+							key: 'jsFunction',
+							content: ''
+						}
+					],
+					Score: 0,
+					Parent: -1,
+					newQuestion: true
+				}
+
+				this.questions.push(question);
+
+				console.log(this.questions);
+			},
+			deleteQuestion () {
+				this.questions.forEach((item, idx) => {
+					if (item.active) {
+						this.questions.splice(idx, 1);
+
+						if (!item.newQuestion) {
+							this.deletedQuestions.push(item.ID);
+						}
+					}
+				})
+			},
+			infoLog () {
+				console.log(this.questions);
+			}
 		}
 	}
-  }
 </script>
