c1614eb6c8a286ed1b9003b622cfab98a998dc92
cosmindanielradu19 <cosmindanielradu19@gmail.com>
2020-01-10 14:13:13 +0200

    finished tasks

--- source/plugins/ctf.admin/data/frontend/js/tasks.js
+++ source/plugins/ctf.admin/data/frontend/js/tasks.js
@@ -11,12 +11,33 @@ new Vue({
         flag: '',
         teams: [],
         serverResponse: '',
-        snackbar1: false
+        snackbar1: false,
+        finishedQuestions: [],
+        canStartTask: false,
+        unfinishedQuestion: '',
+        alreadySolved: false
     },
     mounted() {
         this.$http.get('/api/v1/questions').then(response => {
             this.questions = response.data;
-            console.log(this.questions.length)
+        }, response => {
+            console.log(response);
+        });
+
+        this.$http.get('/api/v1/answers').then(response => {
+            console.log(response.data);
+            if (response.data.err) {
+                console.log(response.data.err);
+            } else {
+                response.data.forEach(element => {
+                    if (element.teamID == localStorage.saveData) {
+                        console.log(this.questions[element.questionID].question);
+                        if (element.finished != undefined) {
+                            this.finishedQuestions.push(this.questions[element.questionID].question);
+                        }
+                    }
+                });
+            }  
         }, response => {
             console.log(response);
         });
@@ -24,7 +45,6 @@ new Vue({
     methods: {
         submit: function () {
             this.questionDialog = false;
-            let teamNameUsed = 'salut';
             this.$http.post('/api/v1/answer/finish', { teamID: localStorage.saveData, questionID: this.currentQuestionID, teamAnswer: this.teamResponse }).then(response => {
                 if (response.data.err) {
                     this.serverResponse = response.data.err;
@@ -32,29 +52,47 @@ new Vue({
                 } else {
                     this.serverResponse = response.data;
                     this.snackbar1 = true;
+                    this.finishedQuestions.push(this.questions[this.currentQuestionID].question);
                 }
-                console.log(response.data);
             });
 
             this.teamResponse = '';
+            this.serverResponse = '';
         },
         taskFunction: function (questionID) {
-            this.currentQuestionID = questionID;
-            this.currentQuestion = this.questions[questionID].question;
-            this.teamResponse = '';
-            this.questionDialog = true;
+            if (this.finishedQuestions.includes(this.questions[questionID].question)) {
+                this.alreadySolved = true;
+                this.canStartTask = false;
+            } else {
+                this.alreadySolved = false;
+                let parent = this.questions[questionID].parent;
+                if (this.finishedQuestions.includes(parent) || parent === -1) {
+                    this.canStartTask = true;
+                    this.currentQuestionID = questionID;
+                    this.currentQuestion = this.questions[questionID].question;
+                    this.teamResponse = '';
+                    this.questionDialog = true;
+                } else {
+                    this.unfinishedQuestion = parent;
+                    this.canStartTask = false;
+                }
+            }
+
         },
         closeDialog: function () {
             this.questionDialog = false;
         },
         startTask() {
-            let teamNameUsed = 'salut';
-            this.$http.post('/api/v1/answer/start', { teamID: localStorage.saveData, questionID: this.currentQuestionID }).then(response => {
-                if (response.data.err) {
-                    this.teamNameUsed = true;
-                    this.serverResponse = response.data.err;
-                }
-            });
+            if (this.canStartTask) {
+                this.$http.post('/api/v1/answer/start', { teamID: localStorage.saveData, questionID: this.currentQuestionID }).then(response => {
+                    if (response.data.err) {
+                        this.teamNameUsed = true;
+                        this.serverResponse = response.data.err;
+                    }
+                });
+            } else {
+                this.snackbar1 = true;
+            }
         }
     }
 });
