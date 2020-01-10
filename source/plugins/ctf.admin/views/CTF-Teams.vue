744563e1d504c685a27b829a7f0486c0a5d0a20c
skyplane23 <moraruliviunicolae1@gmail.com>
2020-01-10 14:01:26 +0200

    team info table added

---
+++ source/plugins/ctf.admin/views/CTF-Teams.vue
@@ -0,0 +1,54 @@
+<template>
+	<div>
+		<v-card
+			class="mx-auto"
+			:max-width="this.width"
+			outlined
+		>
+			<v-card-title>
+				<span class="headline">CTF Teams</span>
+				<v-spacer></v-spacer>
+			</v-card-title>
+			<v-card-text>
+				<v-data-table
+					:headers="headers"
+					:items="teams"
+					:sort-by="['id']"
+					:sort-desc="[false]"
+					class="elevation-1"
+				></v-data-table>
+			</v-card-text>
+			<v-card-actions>
+				<v-spacer></v-spacer>
+				<v-btn @click="closeDialog" color="error">Close</v-btn>
+			</v-card-actions>
+		</v-card>
+	</div>
+</template>
+
+<script>
+  export default {
+	name: 'Teams',
+	props: ['width', 'activeDb'],
+    data () {
+      return {
+        headers: [
+          	{ text: 'ID', value: 'id' },
+          	{ text: 'Team Name', value: 'teamName' },
+          	{ text: 'Board ID', value: 'boardID' },
+			{ text: 'Score', value: 'score' }
+        ],
+        teams: []
+      }
+	},
+	async created () {
+		this.teams = await this.studio.ctf_admin.getTeamsInfo(this.activeDb + '.sqlite');
+		console.log(this.teams);
+	},
+	methods: {
+		closeDialog() {
+			this.$root.$emit('submit');
+		}
+	}
+  }
+</script>
