700a2d7cad49319fae708fcfe89f3b1f0fcabaf5
Serban Andrei <usadekall@gmail.com>
2021-07-06 13:49:15 +0300

    Added dialogs from micropython to flash

--- source/plugins/flash/index.js
+++ source/plugins/flash/index.js
@@ -1,6 +1,9 @@
-let studio = null;
-
 import FlashSelectDevice from './views/FlashSelectDevice.vue';
+import EdgeOrChrome from '../devices/mp/views/EdgeOrChrome.vue';
+import UpgradeToHttps from '../devices/mp/views/UpgradeToHttps.vue';
+
+let studio = null;
+let serialport = null;
 
 let flash = {
 	
@@ -9,8 +12,33 @@ let flash = {
 export default function setup (options, imports, register)
 {
 	studio = imports;
+	serialport = imports.serialport;
 
-	studio.workspace.registerMenuItem ('TOOLBAR_FLASH', 20, () => studio.workspace.showDialog(FlashSelectDevice,{width:500}));
+	studio.workspace.registerMenuItem ('TOOLBAR_FLASH', 20, () => {
+		if(serialport.isAvailable ())
+			studio.workspace.showDialog(FlashSelectDevice,{width:500});
+		else {
+			let chrome = !!window.chrome;
+			let https = (location.protocol === 'https:');
+
+			if(chrome == false) 
+			{
+				studio.workspace.showDialog (EdgeOrChrome, {
+					width: '500px'
+				});
+			} else if(https == false) 
+			{
+				studio.workspace.showDialog (UpgradeToHttps, {
+					width: '500px'
+				});
+			} else 
+			{			
+				studio.workspace.showDialog (EdgeOrChrome, {
+					width: '500px'
+				});
+			}
+		}
+	});
 	
 	register (null, {
 		flash: flash
